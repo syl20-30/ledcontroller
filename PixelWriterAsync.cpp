ae1ca569737795211e6c8d8cbcf861bd38216f9c
Sylvain <79336353+syl20-30@users.noreply.github.com>
2023-01-01 15:44:58 +0100

    Update PixelWriterAsync.cpp

--- PixelWriterAsync.cpp
+++ PixelWriterAsync.cpp
@@ -16,6 +16,7 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public
 License along with NeoPixel.  If not, see
 <http://www.gnu.org/licenses/>.
+
 -------------------------------------------------------------------------*/
 
 #ifdef ARDUINO_ARCH_ESP8266
